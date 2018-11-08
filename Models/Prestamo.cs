ccbd60237789c7c3b963f729636530a9ddc8aff0
cristobal96 <44531415+cristobal96@users.noreply.github.com>
2018-11-07 23:55:21 -0500

    Update Prestamo.cs

--- Models/Prestamo.cs
+++ Models/Prestamo.cs
@@ -8,9 +8,12 @@ namespace BIBLIOTECA.Models
     {
         [Key]
         public int cod_prest {get; set;}
+        //DatosLibro
         public Libro datosLibro {get; set;}
+        //DatosEstudiante
         public Estudiante datosEstudiante {get; set;}
         [Required(ErrorMessage = "Ingresar modalidad")]
+        //Modalidad
         public string modalidad {get; set;}
         [Required(ErrorMessage = "Ingresar fecha de reserva")]
         public DateTime fecha_reserva {get; set;}
