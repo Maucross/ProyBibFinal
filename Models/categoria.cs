7419330ee92b2b0550dd3081d9916b80e87171f7
cristobal96 <44531415+cristobal96@users.noreply.github.com>
2018-11-08 16:56:03 -0500

    Update categoria.cs

--- Models/categoria.cs
+++ Models/categoria.cs
@@ -6,7 +6,9 @@ namespace BIBLIOTECA.Models
 {
     public class Categoria
     {
+        //Id
         public int Id {get; set;}
+        //Nombre
         public string Nombre {get; set;}
         public List<Libro> Libros {get; set;}
 
